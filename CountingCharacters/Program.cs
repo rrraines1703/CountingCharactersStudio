06e1b7fa048b9bb8a46727911a7041c06950b358
Richard Raines <rrraines1703@me.com>
2020-06-02 19:30:05 -0400

    Completed studio.

--- CountingCharacters/Program.cs
+++ CountingCharacters/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace CountingCharacters
 {
@@ -6,7 +8,23 @@ namespace CountingCharacters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
+			Console.WriteLine("Please enter a sentence:");
+
+			string userInput = (Console.ReadLine()).ToUpper();
+			string input = Regex.Replace(userInput, "[^A-Za-z0-9]", "");
+			Dictionary<char, int> characters = new Dictionary<char, int>();
+
+			foreach (var let in input)
+			{
+				if (characters.ContainsKey(let))
+					characters[let] = characters[let] + 1;
+				else
+					characters.Add(let, 1);
+			}
+
+
+			foreach (var item in characters)
+				Console.WriteLine($"{item.Key}: {item.Value}");
+		}
+	}
 }
